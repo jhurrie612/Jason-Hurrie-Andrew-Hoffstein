b9110d29d9852c7e2b3ed5c92372c3d6b19e0f62
ahoffstein <97980619+ahoffstein@users.noreply.github.com>
2022-04-25 15:00:15 -0400

    Final Project: worked on the detail2 page.
    finished detail2 page

---
+++ detail2.php
@@ -0,0 +1,32 @@
+<?php
+include 'top.php';
+?>
+    <main>
+        <h1>Top Wide Receivers</h1>
+            <section class="wr">
+                <h2>Cooper Kupp</h2>
+                <p>This season, Cooper Kupp, the starting wide receiver for the Los Angeles Rams scored a total of 439.5 PPR fantasy points this season, which is nearly 200 points more than he had two seasons before. Over the span of 18 weeks, Cooper Kupp scored at least 30 PPR fantasy points 6 times. In week 14, the Rams played against the Cardinals where Cooper Kupp racked in 13 catches for 123 yards and a touchdown making his total fantasy points 31.3 for the week. The following week, Cooper Kupp put up another 30+ point performance. Against the Seahawks, Kupp caught the ball 9 times for 136 yards and 2 touchdowns scoring 34.7 points for his fantasy owners.</p> 
+                
+                <p>Along with being the highest scoring wide receiver during this fantasy football season, Cooper Kupp was named the Super Bowl MVP after winning Super Bowl LVI with the Rams. In addition, he as named NFL Offensive Player of the Year, First-team All-Pro, and a pro bowler. He also led the league in receptions, receiving yards, and receiving touchdowns. He also holds the record now for Most yards from scrimmage by a wide receiver in a single season with 1,965 yards and Most receiving yards by a wide receiver in a single season including postseason with 2,425 yards.</p>
+
+                <p>The once third round NFL draft pick in 2017 by the Rams, Cooper Kupp, has surprised many with his 2021 Triple Crown and Super Bowl MVP season after leading the league in receptions, receiving touchdowns, and receiving yards for the first time since Steve Smith Sr. did it in 2005. Though many people choose a variety of different wide receivers with their first few picks in their fantasy football drafts including his teammate Robert Woods, it is clear that he is one of the best wide receivers in the game and should go in the first two rounds in most drafts this upcoming season. </p>
+
+                <h2>Randy Moss</h2>
+                <p>In 2007, Randy Moss, one of the best wide receivers in NFL history, joined the New England Patriots and the greatest quarterback of all time, Tom Brady, for an unbelievable season. This year, he generated a total 385.3 PPR points in fantasy football catching the ball 98 times for 1,493 yards and 23 total touchdowns. Throughout this season, the Patriots didn't lose a single regular season game. In week 11 against the Bills, during one of their many wins, Randy Moss was targeted by Tom Brady 13 times, caught the ball 10 times for 128 yards and scored 4 touchdowns racking in a total 46.8 total PPR fantasy points that week. In addition to this 46 point performance, Randy Moss scored over 30 points in a single week 4 other times.</p>
+
+                <p>During this season, Randy Moss was named the NFL Comeback Player of the Year, a First-team All-Pro player, a Pro Bowler, while leading the NFL in receiving touchdowns for the 4th time in his career at that point. He holds the record for most touchdown receptions in a season with 23, most games in one season with at least 2 touchdowns which he did 8 times during this season, and  played on the  highest-scoring team, for a single season, in NFL history that season: The 2007 New England Patriots who had scored 589 points that season.</p>
+
+                <p>Not only is Randy Moss the highest scoring fantasy football wide receiver in one season of all time in 2020, he was also number 5 on the all time list when he scored 376 total PPR points in fantasy during the 2003 season with the Vikings. That season, he caught the ball 111 times for a total of 1,632 yards and 17 total touchdowns. The only other player to appear twice on top 10 list of all time is Marvin Harrison and Antonio Brown. Since the NFL has added a regular season game making it 18 total games rather then 17 like it was for Randy Moss back in 2007, scoring 385+ fantasy football points in a single season is incredible and it will happen more often then it did before. This record was destroyed in 2021 by Cooper Kupp and it may happen again next year.</p>
+            </section>
+
+            <section class="photo">
+                <h2>Photograph</h2>
+                <figure class="kupp">
+                    <img class="rounded" alt="Cooper Kupp SuperBowl MVP" src="images/cooper-kupp.jpeg">
+                    <figcaption><cite><a href="https://thesportswave.net/wp-content/uploads/2022/02/kupp.jpeg" target="_blank">Cooper Kupp SuperBowl MVP</a></cite></figcaption>
+                </figure>
+            </section>
+    </main>
+<?php
+include 'footer.php';
+?>
