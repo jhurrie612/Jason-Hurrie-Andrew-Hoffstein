04341808e3a041fc6dd13c377696aa0f2ff3ae6e
ahoffstein <97980619+ahoffstein@users.noreply.github.com>
2022-04-25 10:55:11 -0400

    Final Project: worked on the detail page.
    Detail page created

---
+++ final/detail.php
@@ -0,0 +1,32 @@
+<?php
+include 'top.php';
+?>
+    <main>
+        <h1>Top Running Backs</h1>
+            <section class="rb">
+                <h2>Jonathan Taylor</h2>
+                <p>In the 2021 NFL season, Jonathan Taylor was leading PPR scorer during this fantasy football season. This season, Jonathan Taylor scored 373.1 PPR points which racking up a league leading 1,811 rushing yards on 332 attempts, averaging 5.5 yards per carry while scoring 18 touchdowns and totaling 40 receptions and adding another 659 receiving yards to his total. Along with this, he was averaging 106.5 yards per game.</p> 
+                
+                <p>This season in week 5, Jonathan Taylor and the Colts played against the Ravens. In this game, Jonathan Taylor rushed 15 times for 53 yards and a touchdown. Along with this, he racked in 116 receiving yards and another receiving touchdown scoring 28.9 fantasy points for his owners that week. In the following week, the colts played against the Texans where he ran 14 times for 145 yards averaging 10.4 yards per carry. There, he scored 27.8 points that week for his owner.In back to back weeks, he scored over 25 points. But this was nothing. In week 11, the colts played against the bills. This was his best game of the season. He ran the ball a season high 32 times for a season high 185 yards and a season high 4 touchdowns in one game. Add on another 19 receiving yards and a receiving touchdown and Jonathan Taylor scored 50.4 fantasy points that week for his owners.</p>
+
+                <p>While being the number one fantasy player this year, Jonathan Taylor also was nominated to the First-Team All-Pro, the Pro Bowl, along with being the rushing yards and rushing touchdown leader. He carried his team to a 9-8 record but it was not enough and the colts unfortunately did not make the playoffs. Next year, Jonathan Taylor is projected to go in one of the top 5 picks of many fantasy football drafts next year because of this amazing season and some even think he could go number one.</p>
+
+                <h2>LaDainian Tomlinson</h2>
+                <p>In the 2006 NFL season, LaDainian Tomlinson was able to score 483.1 PPR points during the entire season. That year, he was able to rack in 1,815 rushing yards, 56 receptions, 508 receiving yards, and 33 total touchdowns, two of these being passing touchdowns, while playing the then San Diego Chargers. The Chargers were able to ride LaDainian Tomlinson amazing season generating a inning record of 14-2, 5 wins more than the previous season. As the best team in the AFC West, they lost to the New England Patriots 21-14 in the Divisional Playoff round.</p>
+
+                <p>While becoming the highest scoring running back in Fantasy Football history during a single season, LaDainian Tomlinson set NFL records by scoring 14 touchdowns in a span of 4 games, 16 touchdowns in span of 5 games, and 19 touchdowns in a span of 6 games. In addition to this, he set a franchise record 4 touchdowns in games against the San Francisco 49ers, Cincinnati Bengals, and Denver Broncos.  He is the first to score three TDs in three straight games and became the first to score at least 3 in four straight games set the following week. Also, he became the second to have three games of four or more TDs in one season. He also became the fastest player ever to score 100 touchdowns.</p>
+
+                <p>This season, LaDainian Tomlinson not only scored the most points ever in Fantasy Football by a running back during a full NFL season, he also won the leagues Most Valuable Player award, the NFL Offensive Player of the Year, was named to the First-team All-Pro, along with being the rushing yards, rushing touchdowns, and scoring leader during the whole 2006 NFL season. LaDainian Tomlinson total 483.1 PPR point season allowed fantasy owners that season to make tons of mistakes during draft night, trades, and more and still help their team make the playoffs. Though some owners that season drafted players such as Larry Johnson and Shaun Alexander ahead of him that season, neither of these players scored touchdowns and and racked up total yards each week like LaDainian Tomlinson did that season.</p>
+            </section>
+
+            <section class="photo">
+                <h2>Photograph</h2>
+                <figure class="jt">
+                    <img class="rounded" alt="Jonathan Taylor Stiff Arms the Defender" src="images/JTStiffArm.jpg">
+                    <figcaption><cite><a href="https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/1312/2021/03/01120424/GettyImages-1294353473-818x460.jpg" target="_blank">Jonathan Taylor Stiff Arms the Defender</a></cite></figcaption>
+                </figure>
+            </section>
+    </main>
+<?php
+include 'footer.php';
+?>
